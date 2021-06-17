69364929bacf50fa6555cc807b86b3fa0bc19e4e
Jake Foster <jake.foster@kainos.com>
2021-06-17 12:32:21 +0100

    created project class

---
+++ src/main/java/Project.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class Project {
+    private int projectID;
+    private Date startDate;
+    private Date endDate;
+    private String projectName;
+
+    public Project() {
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(int projectID) {
+        this.projectID = projectID;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+
+}
